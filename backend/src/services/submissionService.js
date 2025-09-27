c90e345d53ad6d76e8846a0ba18a5d80a5e91b31
Jewel Joseph <jeweljoseph016@gmail.com>
2025-09-27 17:26:05 +0530

    backend initital changes

---
+++ backend/src/services/submissionService.js
@@ -0,0 +1,9 @@
+export class SubmissionService {
+
+    validateInputCode(input) {
+        const isValid = false;
+        const errors = [];
+        // write the logic for validating a string of code
+        return { isValid, errors };
+    }
+}
